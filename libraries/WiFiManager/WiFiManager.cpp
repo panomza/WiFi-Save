89d22e9dcce94cbcc39bb43227a1d59c9fe0fdee
wattanakorn495 <41609635+wattanakorn495@users.noreply.github.com>
2020-04-21 16:33:58 +0700

    Update WiFiManager.cpp
    disable captive portal

--- libraries/WiFiManager/WiFiManager.cpp
+++ libraries/WiFiManager/WiFiManager.cpp
@@ -865,13 +865,13 @@ void WiFiManager::handleNotFound() {
 
 /** Redirect to captive portal if we got a request for another domain. Return true in that case so the page handler do not try to handle the request again. */
 boolean WiFiManager::captivePortal() {
-  if (!isIp(server->hostHeader()) ) {
-    DEBUG_WM(F("Request redirected to captive portal"));
-    server->sendHeader("Location", String("http://") + toStringIp(server->client().localIP()), true);
-    server->send ( 302, "text/plain", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
-    server->client().stop(); // Stop is needed because we sent no content length
-    return true;
-  }
+//   if (!isIp(server->hostHeader()) ) {
+//     DEBUG_WM(F("Request redirected to captive portal"));
+//     server->sendHeader("Location", String("http://") + toStringIp(server->client().localIP()), true);
+//     server->send ( 302, "text/plain", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+//     server->client().stop(); // Stop is needed because we sent no content length
+//     return true;
+//   }
   return false;
 }
 
